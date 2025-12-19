7e392ee82819452057e467dadac0144253beb7a7
nitil5917-cpu <nitil5917@gmail.com>
2025-12-19 07:51:29 +0530

    rename database.py.py to database.py

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 import sqlite3
 
-# Database connect karo
-conn = sqlite3.connect("users.db")  # same file name
+conn = sqlite3.connect("users.db") 
 cursor = conn.cursor()
 
 print("Current Users in DB:")
 
-# Users table se data fetch karo
 cursor.execute("SELECT * FROM users")
 for r in cursor.fetchall():
     print(r)
 
 conn.close()
+
